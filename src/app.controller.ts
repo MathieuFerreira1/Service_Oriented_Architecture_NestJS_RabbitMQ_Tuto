ee9fab540fc0436c03600a8e0a51e69cd4492e19
Mathieu Ferreira <113102754+MathieuFerreira1@users.noreply.github.com>
2024-10-29 14:48:13 +0200

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,18 +7,18 @@ import { ProducerService } from './producer.service';
 export class AppController {
   constructor(private readonly producerService: ProducerService) {}
 
-  // Endpoint pour envoyer un message via RabbitMQ
+  // HTTP endpoint to send a message to RabbitMQ
   @Get('send')
   async sendMessage() {
-    const message = { text: 'Bonjour de RabbitMQ!' };
+    const message = { text: 'Hello from RabbitMQ!' };
     const response = await this.producerService.sendMessage('message_print', message);
     return response;
   }
 
-  // Consommateur pour traiter les messages de RabbitMQ avec le pattern 'message_print'
+  // Consumer for RabbitMQ messages with the pattern 'message_print'
   @MessagePattern('message_print')
   handleMessage(@Payload() data: any) {
-    console.log('Message reçu:', data);
-    return 'Message traité avec succès';
+    console.log('Received message:', data);
+    return 'Message processed successfully';
   }
 }
